17f2156dd73740543f6f5121c491db3d587a6fde
Archana D <86797873+Archana-D@users.noreply.github.com>
2021-12-07 16:23:41 +0530

    Added additional checks.....

--- FrstPrjdemo/src/test/java/com/FrstPoject/test2.java
+++ FrstPrjdemo/src/test/java/com/FrstPoject/test2.java
@@ -3,7 +3,16 @@ package com.FrstPoject;
 public class test2 {
 	public static void main(String[] args)
 	{
-		System.out.println("Test is passed");
+		String hungry = "yes";
+		
+		if(hungry == "yes" || hungry == "Y" || hungry == "y")
+		{
+		   System.out.println("Test is passed");
+	    }
+		else {
+			System.out.println("Do work......");
+		}
 	}
-
 }
+
+
